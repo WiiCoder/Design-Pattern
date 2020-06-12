f2de298119da0f5a67515d0ea8a786845fbcbd33
wiiu <dicoholicxxx@gmail.com>
2020-06-12 15:50:42 +0800

    add Composite Reuse Principle

---
+++ src/com/wiiu/principle/compositeReuse/Principle.java
@@ -0,0 +1,19 @@
+package com.wiiu.principle.compositeReuse;
+
+import sun.tools.jconsole.JConsole;
+
+/**
+ * @ClassName Principle
+ * @Description TODO
+ * @Author dicoholic
+ * @Date 2020/6/12 3:24 下午
+ * @Version 1.0
+ *
+ * 合成复用原则
+ *
+ * 尽量使用合成/聚合的方式，而不是使用继承
+ *
+ */
+
+public class Principle {
+}
