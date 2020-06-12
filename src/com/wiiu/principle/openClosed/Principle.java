a2eeb37c719384d11a70f9bf2b278e8decafaee0
wiiu <dicoholicxxx@gmail.com>
2020-06-12 11:30:00 +0800

    add Open Closed Principle

---
+++ src/com/wiiu/principle/openClosed/Principle.java
@@ -0,0 +1,24 @@
+package com.wiiu.principle.openClosed;
+
+/**
+ * @ClassName Principle
+ * @Description TODO
+ * @Author dicoholic
+ * @Date 2020/6/12 11:24 上午
+ * @Version 1.0
+ *
+ * 开闭原则 Open Closed Principle
+ *
+ * 编程中最基础、最重要的设计原则
+ *
+ * 1）一个软件实体如类，模块和函数应该对拓展开放（对提供方），对修改关闭（对适用房）
+ *    用抽象构建框架，用实现拓展细节。
+ * 2）当软件需要变化时，尽量通过拓展软件实体的行为来实现变化，而不是通过修改已有代码
+ *    来实现变化
+ *
+ * 3）编程中遵循其他原则，以及使用设计模式的目的就是遵循开闭原则
+ *
+ */
+
+public class Principle {
+}
